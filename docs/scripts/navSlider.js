c37bf1da1b229e2cc3a6aefaa3b848321ba1753d
Bojidar Tonev <ghost1993ifyy@gmail.com>
2019-08-08 17:47:36 +0300

    Added responsible design menu for navigation and responsible design for index page.

---
+++ docs/scripts/navSlider.js
@@ -0,0 +1,29 @@
+const navSlide = () => {
+    const burger = document.getElementsByClassName('burger')[0];
+    const nav = document.querySelector('.site-nav ul');
+    const navLinks = document.querySelectorAll('.site-nav li');
+
+
+    burger.addEventListener('click', () => {
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ``;
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.4}s`;
+            }
+        });
+
+        //Burger animation
+        burger.classList.toggle('toggle');
+    });
+
+    
+    
+
+}
+
+navSlide();
